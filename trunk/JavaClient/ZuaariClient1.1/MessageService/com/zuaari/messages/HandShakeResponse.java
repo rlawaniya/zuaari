47957a76dd7b754c2f88f2019cb5b4d4d24aa5eb
mail4mani.raj@gmail.com <mail4mani.raj@gmail.com@98ae8527-7e05-a98a-43af-cd2446c5efa2>
2011-03-04 21:10:08 +0000

    Adding client code

---
+++ trunk/JavaClient/ZuaariClient1.1/MessageService/com/zuaari/messages/HandShakeResponse.java
@@ -0,0 +1,35 @@
+package com.zuaari.messages;
+
+import com.zuaari.connectionmanager.ServiceConstants;
+
+
+public class HandShakeResponse extends Message {
+
+	private int handShakeRes;
+	private String sessionId;
+
+	public static int messageID = ServiceConstants.HANDSHAKE_RES_MSG_ID;
+
+	public HandShakeResponse(int handshakeRes) { //,String sessionId
+		super();
+		this.setHandShakeRes(handshakeRes);
+		super.msgNum = messageID;
+	}
+
+	public void setHandShakeRes(int handShakeRes) {
+		this.handShakeRes = handShakeRes;
+	}
+
+	public int getHandshakeRes() {
+		return handShakeRes;
+	}
+
+	public String getSessionId() {
+		return sessionId;
+	}
+
+	public void setSessionId(String sessionId) {
+		this.sessionId = sessionId;
+	}
+
+}
