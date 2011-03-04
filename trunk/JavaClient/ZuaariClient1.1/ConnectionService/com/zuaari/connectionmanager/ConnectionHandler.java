cd4893258d27197712fdb5d5ba926e8a89c7b9fa
mail4mani.raj@gmail.com <mail4mani.raj@gmail.com@98ae8527-7e05-a98a-43af-cd2446c5efa2>
2011-03-04 21:01:56 +0000

    Adding client code

---
+++ trunk/JavaClient/ZuaariClient1.1/ConnectionService/com/zuaari/connectionmanager/ConnectionHandler.java
@@ -0,0 +1,13 @@
+package com.zuaari.connectionmanager;
+
+import com.zuaari.messages.Message;
+
+public class ConnectionHandler implements IConnectionHandler{
+
+	@Override
+	public void sendMessage(Message msg) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
---
+++ trunk/JavaClient/ZuaariClient1.1/ConnectionService/com/zuaari/connectionmanager/ServiceConstants.java
@@ -0,0 +1,9 @@
+package com.zuaari.connectionmanager;
+
+public class ServiceConstants {
+	public final static int HANDSHAKE_REQ_MSG_ID=1;
+	public final static int HANDSHAKE_RES_MSG_ID=2;
+	
+	public final static int HANDSHAKE_RES_SUCCESS=1000;
+	public final static int HANDSHAKE_RES_FAILURE=1001;
+}
